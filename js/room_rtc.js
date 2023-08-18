b79254290b065edcd0c83ba8325962a40eee42b2
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-08-18 10:57:52 +0530

    remove code

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -150,55 +150,6 @@ let toggleCamera = async (e) => {
     }
 }
 
-// let toggleScreen = async (e) => {
-//     let screenButton = e.currentTarget
-//     let cameraButton = document.getElementById('camera-btn')
-
-//     if (!sharingScreen) {
-//         sharingScreen = true
-
-//         screenButton.classList.add('active')
-//         cameraButton.classList.remove('active')
-//         cameraButton.style.display = 'none'
-
-//         //get the screen sharing track
-//         localStreamTracks = await AgoraRTC.createScreenVideoTrack()
-//         document.getElementById(`user-container-${uid}`).remove()
-//         displayFrame.style.display = 'block';
-
-//         let player = `<div 
-//         class="video__container" id="user-container-${uid}">
-//         <div class="video-player" id="user-${uid}"></div>
-//         </div>`
-
-//         displayFrame.insertAdjacentHTML('beforeend', player);
-//         document.getElementById(`user-container-${uid}`).addEventListener('click', expandVideoFrame);
-
-//         userIdInDisplayFrame = `user-container-${uid}`
-//         localStreamTracks.play(`user-${uid}`)
-
-//         await client.unpublish([localTracks[1]])
-//         await client.publish([localStreamTracks])
-
-//         let videoFrames = document.getElementsByClassName('video__container')
-//         for (let i = 0; videoFrames.length > i; i++) {
-//             if (videoFrames[i].id != userIdInDisplayFrame) {
-//                 videoFrames[i].style.height = '100px';
-//                 videoFrames[i].style.width = '100px';
-//             }
-//         }
-
-//     } else {
-//         sharingScreen = false
-
-//         cameraButton.style.display = 'block'
-//         document.getElementById(`user-container-${uid}`).remove()
-//         await client.unpublish([localStreamTracks])
-
-//          switchToCamera()
-//     }
-// }
-
 let toggleScreen = async (e) => {
     let screenButton = e.currentTarget
     let cameraButton = document.getElementById('camera-btn')
